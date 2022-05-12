5d33696f9256e22b7228e8dd9a818848e98c1f2c
Knapeto <knapeto@gmail.com>
2022-05-12 23:39:36 +0200

    admin

--- apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    includes: ["./app/src/**/*"],
-    service: {
-      name: "delivery-notes-api",
-      localSchemaFile: "./api/generated-schema.graphql",
-    },
-  },
-};
