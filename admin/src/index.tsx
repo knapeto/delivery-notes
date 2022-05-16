f0dc497f443e7e6c08dc0c1eab66ebc92a1cec10
Knapeto <knapeto@gmail.com>
2022-05-16 17:23:22 +0200

    fix

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,28 +1,23 @@
-import './index.css';
+import "./index.css";
 
-import { ApolloProvider } from '@apollo/client';
-import App from './App';
-import { LoadScript } from '@react-google-maps/api';
-import ReactDOM from 'react-dom';
-import createApolloClient from './libs/createApolloClient';
-import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone';
-import utc from 'dayjs/plugin/utc';
+import { ApolloProvider } from "@apollo/client";
+import App from "./App";
+import ReactDOM from "react-dom";
+import createApolloClient from "./libs/createApolloClient";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 dayjs.extend(timezone);
 dayjs.extend(utc);
 
-export const url = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+export const url = process.env.REACT_APP_API_URL || "http://localhost:3000";
 
-const client = createApolloClient(url + '/admin/graphql');
+const client = createApolloClient(url + "/admin/graphql");
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <LoadScript
-      libraries={['places']}
-      googleMapsApiKey="AIzaSyDD2xolNUFyUhWnPDXJo1hi0pjhFpw85vY">
-      <App client={client} />
-    </LoadScript>
+    <App client={client} />
   </ApolloProvider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
