46a3e2f8dbd57a964fa09d25e6b3ff50d2e558da
Knapeto <knapeto@gmail.com>
2022-05-18 19:58:08 +0200

    fixes

--- admin/src/modules/deliveryNotes/index.tsx
+++ admin/src/modules/deliveryNotes/index.tsx
@@ -2,6 +2,7 @@ import { Button, FormControl, InputLabel, OutlinedInput } from "@mui/material";
 
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import { CircularProgress } from "@material-ui/core";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -11,12 +12,19 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Title } from "react-admin";
 import cookie from "js-cookie";
+import styled from "styled-components";
 import { url } from "../../";
 import { useGetDeliveryNotesLazyQuery } from "../../@generated/hooks";
 import { useState } from "react";
 
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 export const DeliveryNotesList = () => {
-  const [search, setSearch] = useState("5006905"); // 5006905
+  const [search, setSearch] = useState("");
+  const [loadingIds, setLoadingIds] = useState([]);
 
   const [getDeliveryNotes, { data }] = useGetDeliveryNotesLazyQuery();
 
@@ -28,28 +36,40 @@ export const DeliveryNotesList = () => {
     });
   };
 
+  const handleSetLoading = (id) => {
+    if (loadingIds.includes(id)) {
+      setLoadingIds(loadingIds.filter((item) => item !== id));
+    } else {
+      setLoadingIds([...loadingIds, id]);
+    }
+
+    setTimeout(() => {
+      setLoadingIds(loadingIds.filter((item) => item !== id));
+    }, 10000);
+  };
+
   return (
     <div>
-      <Card style={{ width: 400, marginLeft: "30%" }}>
+      <Card style={{ width: "40%" }}>
         <Title title="Delivery note" />
         <CardContent>
           <form onSubmit={onSubmit}>
-            <FormControl size="small" style={{ marginTop: 20, width: "100%" }}>
-              <InputLabel>Search</InputLabel>
-              <OutlinedInput
-                type="text"
-                size="small"
-                label="Search"
-                value={search}
-                onChange={(event) => setSearch(event.target.value)}
-              />
-            </FormControl>
+            <Row>
+              <FormControl size="small" style={{ width: "100%" }}>
+                <InputLabel>Search</InputLabel>
+                <OutlinedInput
+                  type="text"
+                  size="small"
+                  label="Search"
+                  value={search}
+                  onChange={(event) => setSearch(event.target.value)}
+                />
+              </FormControl>
 
-            <br />
-
-            <Button onClick={onSubmit} style={{ width: "100%" }}>
-              Search
-            </Button>
+              <Button onClick={onSubmit} style={{ width: 150, marginLeft: 22 }}>
+                Search
+              </Button>
+            </Row>
           </form>
         </CardContent>
       </Card>
@@ -62,14 +82,14 @@ export const DeliveryNotesList = () => {
             <TableRow>
               <TableCell align="right">Klient</TableCell>
               <TableCell align="right">Fakturační místo</TableCell>
-              <TableCell align="right">Adresa dodání</TableCell>
-              <TableCell align="right">Město dodání</TableCell>
-              <TableCell align="right">Země dodání</TableCell>
-              <TableCell align="right">Číslo DL</TableCell>
-              <TableCell align="right">Objednávka</TableCell>
-              <TableCell align="right">Objednávka zákazníka</TableCell>
+              <TableCell align="right">Místo dodání</TableCell>
+              <TableCell align="right">Dodací list</TableCell>
+              <TableCell align="right">Obj</TableCell>
+              <TableCell align="right">Obj zákazníka</TableCell>
               <TableCell align="right">Code</TableCell>
-              <TableCell align="right">Akce</TableCell>
+              <TableCell align="right" width="120">
+                Akce
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -80,9 +100,11 @@ export const DeliveryNotesList = () => {
               >
                 <TableCell align="right">{row.KlientNazev}</TableCell>
                 <TableCell align="right">{row.FakturacniMistoNazev}</TableCell>
-                <TableCell align="right">{row.DodaciMisto_Nazev}</TableCell>
-                <TableCell align="right">{row.DodaciMisto_Mesto}</TableCell>
-                <TableCell align="right">{row.DodaciMisto_Zeme}</TableCell>
+                <TableCell align="right">
+                  <div>{row.DodaciMisto_Nazev}</div>
+                  <div>{row.DodaciMisto_Mesto}</div>
+                  <div>{row.DodaciMisto_Zeme}</div>
+                </TableCell>
                 <TableCell align="right">{row.CisloDL}</TableCell>
                 <TableCell align="right">{row.CisloObjKlienta}</TableCell>
                 <TableCell align="right">{row.CisloObjZakaznika}</TableCell>
@@ -90,13 +112,22 @@ export const DeliveryNotesList = () => {
                 <TableCell align="right">
                   <a
                     target="_blank"
+                    onClick={() =>
+                      handleSetLoading(row.InterniCisloDokladuProTisk)
+                    }
                     href={`${url}/admin/delivery-note/${
                       row.InterniCisloDokladuProTisk
                     }?access_token=${cookie.get("access_token")}`}
                     download
                     style={{ margin: "0 auto" }}
                   >
-                    Download
+                    {loadingIds.includes(row.InterniCisloDokladuProTisk) ? (
+                      <CircularProgress
+                        style={{ height: 20, width: 20, marginRight: 24 }}
+                      />
+                    ) : (
+                      "Download"
+                    )}
                   </a>
                 </TableCell>
               </TableRow>
--- api/src/modules/deliveryNotes/delivery-notes.controller.ts
+++ api/src/modules/deliveryNotes/delivery-notes.controller.ts
@@ -19,28 +19,29 @@ export class DeliveryNotesController {
 
     try {
       const response = await this.httpService.axiosRef({
-        url: `http://172.18.1.9:8080/jasperserver/rest_v2/reports/Tiskove_sestavy/EXPET01ST.pdf?PKVENR=${deliveryId}`,
+        // url: `http://172.18.1.9:8080/jasperserver/rest_v2/reports/Tiskove_sestavy/EXPET01ST.pdf?=PKVENR=${deliveryId}`, //AKAPN
+        url: `http://172.18.1.9:8080/jasperserver/rest_v2/reports/Tiskove_sestavy/DANONE01ST.pdf?APN=${deliveryId}&USR=ELGBORNADP`, //AKAPN
         method: 'GET',
         auth: {
           username: 'flowForce',
           password: 'abTqHCHL1cXh5iJ8Hyr8HTrsgzXWtUm',
         },
+        responseType: 'stream',
       });
 
-      console.log(response.data)
-
-      // response.data.pipe(writer);
+      response.data.pipe(writer);
 
       return new Promise((resolve, reject) => {
         writer.on('finish', () => {
-          console.log('aa')
           const file = createReadStream(fileName);
           file.pipe(res);
+
+          fs.unlinkSync(fileName);
         });
-        // writer.on('error', error => console.log(error));
+        writer.on('error', (error) => console.log('error', error));
       });
-    } catch(error) {
-      console.log(error.toJSON())
+    } catch (error) {
+      console.log(error.toJSON());
     }
   }
 }
